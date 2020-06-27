ef6eb6f4511d68a0efe7891171de0870975012d8
AzizAlhaider <azizalhaider03@gmail.com>
2020-06-27 20:33:47 +0300

    Azizalhaider

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,31 @@
 // السؤال الأول
 // 1
-
+const name = "انا عبدالعزيز و عمري ";
 // 2
-
+const age = 16;
 // 3 and 4
-
+console.log(name+" "+age);
 // 5 and 6
-
+console.log(age *5)
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobbies = ["كرة القدم ","السباحة"] 
 // 2
-
+hobbies.push(16)
+console.log(hobbies)
 // 3
-
+hobbies.pop()
+console.log(hobbies)
 // 4
-
+let student = {road:"web",lang:"javascript"}
 // 5
-
+student.ta = ["rahaf","moudhi","nancy","hussain"];
 // 6
-
+console.log(student.road)
 // 7
-
+console.log("rahaf")
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
